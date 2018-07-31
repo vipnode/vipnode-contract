99d595d1b32c236cb04bd0056775d4b1f7809ac4
Andrey Petrov <andrey.petrov@shazow.net>
2018-07-31 13:29:08 -0400

    vipnode-pool: Fleshing out the contract meat

--- contracts/VipnodePool.sol
+++ contracts/VipnodePool.sol
@@ -23,14 +23,20 @@ contract VipnodePool {
   // the balance and unlock it for withdrawing.
   event ForceSettle(
     address client,
-    uint timestamp
+    uint256 timeLocked
+  );
+
+  // Balance is emitted whenever a client balance is updated.
+  event Balance(
+    address client,
+    uint256 balance
   );
 
   // Client represents a Pool client who pays to connect to a Pool host.
   struct Client {
-    bytes32[] nodes; // Whitelisted nodes by Client
-    uint balance; // Client's balance
-    uint timeLocked; // If not 0, then client will be allowed to withdraw balance after this time.
+    bytes32[] nodeIDs; // Whitelisted nodes by Client
+    uint256 balance; // Client's balance
+    uint256 timeLocked; // If not 0, then client will be allowed to withdraw balance after this time.
   }
 
   // operator is the pool operator who has permission to manage the client balances.
@@ -38,7 +44,7 @@ contract VipnodePool {
 
   // Amount of time the pool manager has to settle the balance before the
   // client can force a balance withdraw.
-  uint public withdrawInterval = 7 days;
+  uint256 public withdrawInterval = 7 days;
 
   // Mapping of owner -> client.
   mapping (address => Client) clients;
@@ -51,8 +57,15 @@ contract VipnodePool {
 
   // addBalance adds the msg.sender as a Client and adds the nodeID to the node
   // whitelist. If nodeID is 0, whitelisting is skipped.
-  function addBalance(bytes32 nodeID) payable {
-    // XXX: TODO
+  function addBalance(bytes32 _nodeID) public payable {
+    Client storage c = clients[msg.sender];
+    c.balance = c.balance + msg.value;
+
+    if (nodeID != bytes32(0)) {
+      c.nodeIDs.push(_nodeID)
+    }
+
+    Balance(msg.sender, c.balance);
   }
 
   // forceSettle emits a ForceSettle event for the msg.sender if it's a valid
@@ -60,20 +73,36 @@ contract VipnodePool {
   // and release the balance funds.
   // The client's timeLocked value will initialize to +withdrawInterval, after
   // which the client will be allowed to forceWithdraw.
-  function forceSettle() {
-    // XXX: TODO
+  // Requires the client's balance to be >0.
+  function forceSettle() public {
+    Client storage c = clients[msg.sender];
+    require(c.balance > 0);
+
+    c.timeLocked = this.withdrawInterval + block.timestamp;
+
+    ForceSettle(msg.sender, c.timeLocked)
   }
 
   // forceWithdraw allows the msg.sender to withdraw any available client
-  // balance iff timeLocked is set and older than now.
-  function forceWithdraw() {
-    // XXX: TODO
+  // balance iff timeLocked is set and older than now and balance > 0.
+  function forceWithdraw() public {
+    Client storage c = clients[msg.sender];
+    require(c.balance > 0);
+    require(c.timeLocked > block.timestamp);
+
+    // Reset client
+    uint256 amount = c.balance;
+    c.balance = 0;
+    c.timeLocked = 0;
+
+    msg.sender.transfer(amount);
+    Balance(msg.sender, c.balance);
   }
 
 
   // removeClientNode removes nodeID from the msg.sender's client's node
   // whitelist.
-  function removeClientNode(bytes32 nodeID) {
+  function removeClientNode(bytes32 _nodeID) {
     // XXX: TODO
   }
 
@@ -81,8 +110,22 @@ contract VipnodePool {
   // it to _operator.
   // If _release, then send the remaining _balance to the _client and set
   // _balance to 0. To be called when ForceSettle is initiated.
-  function settle(address _client, uint _amount, bool _release) {
+  function settle(address _client, uint256 _amount, bool _release) {
+    // FIXME: Confirm that there is no race conflict with addBalance.
     require(msg.sender == operator);
-    // XXX: TODO
+
+    Client storage c = clients[_client];
+    require(c.balance >= _amount)
+    c.balance = c.balance - _amount;
+
+    if (_release) {
+      uint256 amount = c.balance;
+      c.balance = 0;
+      c.timeLocked = 0;
+
+      _client.sender.transfer(amount);
+    }
+
+    Balance(_client, c.balance);
   }
 }
