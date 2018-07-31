79c38093d8b612a307c4f82fdd76711e606d19fc
Andrey Petrov <andrey.petrov@shazow.net>
2018-07-31 16:24:51 -0400

    Test opWithdraw

--- test/pool.js
+++ test/pool.js
@@ -7,16 +7,34 @@ contract('Vipnode Pool', async (accounts) => {
     const clientWallet = accounts[1];
     const instance = await VipnodePool.new(opWallet);
     const nodeID = web3.fromAscii("42");
+    const value = web3.toWei(42, "ether");
 
-    let res = await instance.addBalance(nodeID, { value: 1234, from: clientWallet });
+    let res = await instance.addBalance(nodeID, { value: value, from: clientWallet });
     assert.equal(res.logs[0].event, "Balance");
     let evt = res.logs[0].args;
     assert.equal(evt.client, clientWallet);
-    assert.equal(evt.balance, 1234);
+    assert.equal(evt.balance, value);
 
-    assert.ok(await instance.checkBalance(clientWallet, nodeID, 1200));
-    assert.notOk(await instance.checkBalance(clientWallet, nodeID, 1300));
-    assert.notOk(await instance.checkBalance(opWallet, nodeID, 1200));
-    assert.notOk(await instance.checkBalance(clientWallet, web3.fromAscii("69"), 1200));
+    assert.ok(await instance.checkBalance(clientWallet, nodeID, value - 1000));
+    assert.notOk(await instance.checkBalance(clientWallet, nodeID, value + 1000));
+    assert.notOk(await instance.checkBalance(opWallet, nodeID, value - 1000));
+    assert.notOk(await instance.checkBalance(clientWallet, web3.fromAscii("69"), value - 1000));
+
+    assert.equal(web3.eth.getBalance(instance.address), value);
+
+    try {
+      await instance.opWithdraw(value, { from: clientWallet });
+      assert.fail("failed to throw on bad opWithdraw");
+    } catch(err) {}
+
+    assert.equal(web3.eth.getBalance(instance.address), value);
+    const startBalance = web3.eth.getBalance(opWallet);
+
+    await instance.opWithdraw(value, { from: opWallet });
+    assert.equal(web3.eth.getBalance(instance.address), 0);
+    assert.equal(
+      web3.eth.getBalance(opWallet).toPrecision(5),
+      startBalance.plus(value).minus(res.receipt.cumulativeGasUsed).toPrecision(5), // This math is slightly off for some reason? Is the receipt a lie?
+    );
   })
 })
